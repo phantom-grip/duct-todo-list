ac1de46c6b791fc6af8c30920fdaa11cc1d22b37
Yanis Urbis <yanis.urbis@gmail.com>
2019-03-07 10:02:12 +0300

    Add simple todo list

--- src/todo_list/client.cljs
+++ src/todo_list/client.cljs
@@ -1,9 +1,52 @@
 (ns todo-list.client
   (:require [reagent.core :as r]))
 
-(defn hello-world []
-  [:h1 "Hello world"])
+;; TODO: add ability to delete
+;; TODO: add ability to check/uncheck
+;; TODO: add filters: all, active, completed
+;; TODO: add counter of elements left
+;; TODO: add ability to clear completed elements
+;; TODO: add check/uncheck all elements
+;; TODO: use bulma for styling
 
-(r/render [hello-world]
+(def filters [:all :completed :active])
+(def initial-state {:filter :all
+                    :todos  []})
+
+(def todo-list (r/atom initial-state))
+(defn add-todo [todo]
+  (let [new-todos (-> @todo-list
+                      :todos
+                      (conj {:title todo}))]
+    (swap! todo-list #(assoc % :todos new-todos))))
+(defn get-todos []
+  (:todos @todo-list))
+
+(defn lister [items]
+  [:ul
+   (for [item items]
+     ^{:key item} [:li (:title item)])])
+
+(defn input []
+  (let [value (r/atom "")]
+    (fn []
+      [:form
+       {:on-submit (fn [e]
+                     (do
+                       (.preventDefault e)
+                       (add-todo @value)
+                       (reset! value "")))}
+       [:input {:type      "text"
+                :value     @value
+                :on-change #(reset! value (-> % .-target .-value))}]])))
+
+(defn home-page []
+  (fn []
+    [:div
+     [:h1 "My todos:"]
+     [input]
+     [lister (get-todos)]]))
+
+(r/render [home-page]
           (-> js/document
               (.getElementById "app")))
