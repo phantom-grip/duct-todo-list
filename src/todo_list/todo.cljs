f4ab2f7bc356a133062f6e743b1366d29714037b
Yanis Urbis <yanis.urbis@gmail.com>
2019-03-08 15:22:37 +0300

    Add lib to work with single todo

---
+++ src/todo_list/todo.cljs
@@ -0,0 +1,37 @@
+(ns todo-list.todo
+  (:require [nano-id.core :refer [nano-id]]))
+
+(comment (def sample-todo {
+                           :description "Sample title"
+                           :id          0
+                           :completed   false
+                           }))
+
+(defn create [description]
+  {:description description
+   :id          (nano-id)
+   :completed   false})
+
+(defn equals? [todo1 todo2]
+  (= (:id todo1) (:id todo2)))
+
+(defn toggle [todo]
+  (update todo :completed #(not %)))
+
+(defn complete [todo]
+  (assoc todo :completed true))
+
+(defn uncomplete [todo]
+  (assoc todo :complete false))
+
+(defn completed? [todo]
+  (:completed todo))
+
+(defn active? [todo]
+  (false? (:completed todo)))
+
+(defn get-id [todo]
+  (:id todo))
+
+(defn check-import []
+  [:div "sample test"])
