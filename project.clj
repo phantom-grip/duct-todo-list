6f189e7a465ca7977f55277818f15ab9c7cdfb04
Yanis Urbis <yanis.urbis@gmail.com>
2019-03-08 15:23:16 +0300

    Add nano-id to work with ids

--- project.clj
+++ project.clj
@@ -7,20 +7,21 @@
                  [duct/module.logging "0.4.0"]
                  [duct/module.web "0.7.0"]
                  [duct/module.cljs "0.4.0"]
-                 [reagent "0.8.1"]]
+                 [reagent "0.8.1"]
+                 [nano-id "0.9.3"]]
   :plugins [[duct/lein-duct "0.11.2"]]
   :main ^:skip-aot todo-list.main
-  :uberjar-name  "todo-list-standalone.jar"
+  :uberjar-name "todo-list-standalone.jar"
   :resource-paths ["resources" "target/resources"]
-  :prep-tasks     ["javac" "compile" ["run" ":duct/compiler"]]
-  :middleware     [lein-duct.plugin/middleware]
+  :prep-tasks ["javac" "compile" ["run" ":duct/compiler"]]
+  :middleware [lein-duct.plugin/middleware]
   :profiles
-  {:dev  [:project/dev :profiles/dev]
-   :repl {:prep-tasks   ^:replace ["javac" "compile"]
-          :dependencies [[cider/piggieback "0.3.10"]]
-          :repl-options {:init-ns user
-                         :nrepl-middleware [cider.piggieback/wrap-cljs-repl]}}
-   :uberjar {:aot :all}
+  {:dev          [:project/dev :profiles/dev]
+   :repl         {:prep-tasks   ^:replace ["javac" "compile"]
+                  :dependencies [[cider/piggieback "0.3.10"]]
+                  :repl-options {:init-ns          user
+                                 :nrepl-middleware [cider.piggieback/wrap-cljs-repl]}}
+   :uberjar      {:aot :all}
    :profiles/dev {}
    :project/dev  {:source-paths   ["dev/src"]
                   :resource-paths ["dev/resources"]
